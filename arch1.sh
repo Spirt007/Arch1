edaded31227c89c6be83991deea15909e5b30d06
Spirt007 <otstawnoi-stepan@mail.ru>
2019-03-30 21:58:36 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -59,7 +59,7 @@ mount /dev/sda3 /mnt/home
 swapon /dev/sda2
 
 echo '3.1 Выбор зеркал для загрузки. Ставим зеркало от Яндекс'
-echo "Server = http://mirror.yandex.ru/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+echo "Server = http://mirror.yandex.ru/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
 
 echo '3.2 Установка основных пакетов'
 pacstrap /mnt base base-devel
